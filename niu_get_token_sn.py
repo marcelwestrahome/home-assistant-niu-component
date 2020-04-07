63a6f4ff662df680d5a404bc1feaffba7775dcad
marcelwestrahome <34414405+marcelwestrahome@users.noreply.github.com>
2020-04-07 10:17:41 +0200

    Delete niu_get_token_sn.py

--- niu_get_token_sn.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-from niu import get_token, get_vehicles, get_info
-
-if __name__ == "__main__":
-    token = get_token()
-    sn = get_vehicles(token)
-    print ('Token:         ', token)
-    print ('Serien-Nummer: ', sn)
